e5d65a6ee7eae7201cf2abc0a55f6b94405beb71
Scorpionsss <chenjie_alan@163.com>
2018-12-14 00:44:04 +0800

    activemq消息中间件测试

---
+++ jms-spring/src/main/java/com/imooc/jms/producer/ProducerServiceImpl.java
@@ -0,0 +1,27 @@
+package com.imooc.jms.producer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+
+import javax.annotation.Resource;
+import javax.jms.*;
+
+public class ProducerServiceImpl implements ProducerService {
+
+    @Autowired
+    JmsTemplate jmsTemplate;
+    @Resource(name = "queueDestination")
+    //@Resource(name = "topicDestination")
+    Destination destination;
+
+    public void sendMessage(final String message) {
+        jmsTemplate.send(destination, new MessageCreator() {
+            public Message createMessage(Session session) throws JMSException {
+                TextMessage textMessage = session.createTextMessage(message);
+                return textMessage;
+            }
+        });
+        System.out.println("发送消息" + message);
+    }
+}
